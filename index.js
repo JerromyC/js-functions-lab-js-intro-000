fff1661b710f9e8b5c5c72a5db322eab213e4b4a
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-21 04:41:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo() {
+function happyHolidaysTo(name) {
   return `Happy holidays, ${name}`
 }
 
-function happyCustomHolidaysTo() {
+function happyCustomHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown() {
+function holidayCountdown(days, holiday) {
   return `It's ${days} days until ${holiday}!`
 }
