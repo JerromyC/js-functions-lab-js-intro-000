397e74cd5fbcb7081228c55b22244164d75c6a75
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-21 04:35:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 function happyHolidays() {
-  return "Happy Holidays!";
+  return "Happy holidays!";
 }
 
 function happyHolidaysTo() {
-  return `Happy Holidays, ${name}`;
+  return `Happy holidays, ${name}`;
 }
 
 function happyCustomHolidaysTo() {
